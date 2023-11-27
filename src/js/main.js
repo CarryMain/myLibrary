3aa23481f3ca725efdef9e9cf58e2709f2140f3b
CarryMain <mr.turanov98@gmail.com>
2023-11-27 22:19:56 +0300

    dinamic created modal window

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,37 @@ $('[data-count="second"]').on('click', ()=> {
 
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
-})
+});
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsunf sdjfksdafjaslfkjesdjfwaselfj'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello World!');
+                }
+            ]
+        ]
+    }
+}));

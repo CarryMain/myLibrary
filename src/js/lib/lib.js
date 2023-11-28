ca11c42a89b16b17df310d1c33b9a931f88e8e7f
CarryMain <mr.turanov98@gmail.com>
2023-11-28 10:57:30 +0300

    add tab and accordion

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -7,5 +7,7 @@ import './modules/effects';
 import './components/dropdown';
 import './modules/attributes';
 import './components/modal';
+import './components/tab';
+import './components/accordion';
 
 export default $;
